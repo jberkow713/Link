05f507666c6ae016c45e1245c5d9f380e6b6fb5f
jesse berkowitz <jberkow@brandeis.edu>
2023-05-08 14:25:25 -0400

    moving camera

--- level.py
+++ level.py
@@ -6,9 +6,9 @@ from player import Player
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-
-        self.visible_sprites =pygame.sprite.Group()
         self.obstacle_sprites=pygame.sprite.Group()
+        self.visible_sprites =YsortCameraGroup()
+        
         self.create_map()
     def create_map(self):
         for row_idx,row in enumerate(WORLD_MAP):
@@ -20,9 +20,24 @@ class Level:
                 if col == 'p':
                     self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
 
-
-
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-           
+
+class YsortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] //2
+        self.half_height = self.display_surface.get_size()[1] //2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self,player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)    
+
